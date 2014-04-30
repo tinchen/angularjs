a32b5fee697fc34411f4f59ad13b5588695157c2
tin <tin.chen@blogup.com.tw>
2014-04-30 03:37:54 -0400

    Refactoring and add more comments for code

--- client/todos/todos.js
+++ client/todos/todos.js
@@ -1,6 +1,7 @@
-var angular = angular || {};
+/*global angular */
 
-// COLLECTION and MODEL
+var ENTER_KEY = 13;
+
 var TodoList = [];
 
 // 資料從 localStorage 載入，實際上可 AJAX 從 DB 載入
@@ -18,11 +19,46 @@ TodoList.save = function () {
   localStorage.TodoList = JSON.stringify(TodoList);
 };
 
-// 刪除指定的待辦事項
-TodoList.destroy = function (index) {
-  TodoList.splice(index, 1);
+// 加入一個新的待辦事項
+TodoList.add = function ($event) {
+  if ($event.keyCode != ENTER_KEY) return; // 非 Enter keypress 不處理
+  TodoList.push(new Todo(TodoList.newTodo));
+  TodoList.newTodo = '';
 };
+
+// 將所有的事項標為已經完成或未完成
+TodoList.toggle = function () {
+  TodoList.forEach(function (todo) {
+    todo.done = TodoList.allToggled;
+  });
+};
+
+// 刪除指定的待辦事項或所有已經完成的待辦事項
+TodoList.destroy = function (index) {
+  if (index !== undefined) {
+    return TodoList.splice(index, 1);
+  }
   
+  index = TodoList.length;
+  while (index--) {
+    if (TodoList[index].done) {
+      TodoList.splice(index, 1);
+    }
+  }
+};
+
+// 計算尚未完成的事項數目
+TodoList.remaining = function () {
+  return TodoList.$filter('filter')(TodoList, { done: false }).length;
+};
+
+// 計算已經完成的事項數目
+TodoList.completed = function () {
+  return TodoList.$filter('filter')(TodoList, { done: true }).length;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
 var Todo = function (title, order, done) {
   this.title = title;
   this.order = order || TodoList.length + 1;
@@ -42,60 +78,17 @@ var Todo = function (title, order, done) {
   };
 };
 
+////////////////////////////////////////////////////////////////////////////////
+
 // 定義一個 Todos Module 與 TodoCtrl CONTROLLER
 var todosModule = angular.module('Todos', []);
 
-todosModule.controller('TodoCtrl', function ($scope) {
+todosModule.controller('TodoCtrl', function ($scope, $filter) {
+  TodoList.$filter = $filter;
   $scope.todoList = TodoList.load();
+  
   // 當 $scope.todoList 內的資料有任何變更就儲存
   $scope.$watch('todoList', function () {
     TodoList.save();
   }, true);
-
-  // 計算尚未完成的事項數目
-  $scope.remaining = function () {
-    var count = 0;
-    TodoList.forEach(function (todo) {
-      count += (!todo.done) ? 1 : 0;
-    });
-    return count;
-  };
-
-  // 計算已經完成的事項數目
-  $scope.completed = function () {
-    var count = 0;
-    TodoList.forEach(function (todo) {
-      count += (todo.done) ? 1 : 0;
-    });
-    return count;
-  };
-
-  // 加入一個新的待辦事項
-  $scope.add = function ($event) {
-    if ($event.keyCode != 13) return;
-    TodoList.push(new Todo($scope.newTodoTitle));
-    $scope.newTodoTitle = '';
-  };
-
-  // 刪除指定的待辦事項
-  $scope.destroy = function (index) {
-    TodoList.splice(index, 1);
-  };
-
-  // 刪除已經完成的待辦事項
-  $scope.clearCompleted = function () {
-    var index = TodoList.length;
-    while (index--) {
-      if (TodoList[index].done) {
-        TodoList.splice(index, 1);
-      }
-    }
-  };
-
-  // 將所有的事項標為已經完成
-  $scope.toggleAll = function () {
-    TodoList.forEach(function (todo) {
-      todo.done = $scope.allToggled;
-    });
-  };
 });
